b4b3a6b97b52da015f4eafeffc2cb96d7ecac943
kriti <sharmakriti017@gmail.com>
2019-12-03 23:52:14 +0530

    styled header inline

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
- const Header=function()
- {
-     return (
-         <div className="header">
-             Phone Directory
-         </div>
-     )
- }
- export default Header;
+
+ 
+const Header = function () {
+    const headerStyle = {
+        textAlign: 'center',
+        padding: 20,
+        background: '#000',
+        color: '#fff',
+        textTransform: 'uppercase'
+    };
+
+    return (
+       
+        <div style={headerStyle}>
+            Phone Directory
+        </div>
+    )
+}
+export default Header;
