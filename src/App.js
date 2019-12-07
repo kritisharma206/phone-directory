5a851e3d6a105a6e5e0f06ac6583c98e3ae4f813
kriti <sharmakriti017@gmail.com>
2019-12-07 23:46:54 +0530

    Props: Passed 'heading' to Header component as props

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Header from './Header.js';
 import './App.css';
 
- class App extends Component {
-   render() {
+class App extends Component {
+
+  render() {
+
     let subscribers = [
       {
         id: 1,
@@ -16,9 +18,10 @@ import './App.css';
         phone: "9999999999"
       }
     ];
-     return (
-       <div>
-          <Header />
+
+    return (
+      <div>
+        <Header heading="Phone Directory" />
         <div className="component-body-container">
           <button className="custom-btn add-btn">Add</button>
 
@@ -26,23 +29,22 @@ import './App.css';
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
-           {
+
+          {
             subscribers.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
-                 <span className="grid-item action-btn-container">
+                <span className="grid-item action-btn-container">
                   <button className="custom-btn delete-btn">Delete</button>
                 </span>
               </div>
             })
           }
-     </div>
-     </div>
-
-     );
-   }
- }
-
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
