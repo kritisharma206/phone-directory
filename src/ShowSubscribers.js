81e744134c931924271ae99433144f81afdcae74
kriti <sharmakriti017@gmail.com>
2019-12-09 14:19:30 +0530

    Smart & Dumb: Added ‘PhoneDirectory’ as smart component, renamed ‘App…

--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
-import './App.css';
 
-class App extends Component {
+import './ShowSubscribers.css';
 
-  constructor() {
-    super();
-    this.state = {
-      subscribersListToShow: []
-    }
-  }
 
+
+class ShowSubscribers extends Component {
 
   render() {
+    
 
     return (
       <div>
         <Header heading="Phone Directory" />
         <div className="component-body-container">
           <button className="custom-btn add-btn">Add</button>
-
           <div className="grid-container heading-container">
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
 
-          {    this.state.subscribersListToShow.map(sub => {
+          {
+           
+            this.props.subscribersList.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
@@ -41,4 +38,5 @@ class App extends Component {
   }
 }
 
-export default App;
+
+export default ShowSubscribers;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
+import PhoneDirectory from './PhoneDirectory';
 
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 //ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
 registerServiceWorker();
