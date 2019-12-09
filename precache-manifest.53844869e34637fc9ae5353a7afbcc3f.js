50c4c10ca4881baf958288078f58c6cf9a82e9aa
kriti <sharmakriti017@gmail.com>
2019-12-09 15:49:39 +0530

    Updates

---
+++ precache-manifest.53844869e34637fc9ae5353a7afbcc3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d8f3cbde39081ca201bbec9d9bbea6",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "aee520a0e11887803092",
+    "url": "/phone-directory/static/css/main.caf4835d.chunk.css"
+  },
+  {
+    "revision": "7387ca35caaf808b313b",
+    "url": "/phone-directory/static/js/2.01ef6ef3.chunk.js"
+  },
+  {
+    "revision": "aee520a0e11887803092",
+    "url": "/phone-directory/static/js/main.ff36bb3e.chunk.js"
+  },
+  {
+    "revision": "750e5236f65c3a1f5e4c",
+    "url": "/phone-directory/static/js/runtime-main.32348089.js"
+  }
+]);
